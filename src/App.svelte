3e552b695ae61bd4201aa7fdab2d7ef34babd91d
Kirill <publickirill2245@gmail.com>
2025-06-18 03:19:17 +0500

    setting structure

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Main from './lib/components/Main/Main.svelte';
+</script>
+  <div class="app">
+    <Main/>
+  </div>
+
+<style>
+</style>
---
+++ src/lib/components/Main/Main.svelte
@@ -0,0 +1,5 @@
+<script>
+    import styles from './styles.module.css'
+</script>
+<main class={styles.main}>
+</main>
